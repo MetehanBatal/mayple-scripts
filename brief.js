2500658b77d794d907d3614205de13cd87b81605
MetehanBatal <metehanbatal@gmail.com>
2022-02-03 16:04:43 +0300

    Form handler

--- brief.js
+++ brief.js
@@ -62,7 +62,7 @@ let briefHench = {
 
 		timeInLA = parseInt(timeInLA);
 		console.log( timeInLA, typeof(timeInLA) );
-		if (17 > timeInLA && timeInLA > 8 ) {
+		if (17 > timeInLA && timeInLA > 7 ) {
 			console.log( 'It is in the range' );
 			document.querySelector('.call-preference-box').classList.remove('hidden');
 		} else {
