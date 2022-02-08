55fc7bf968ef5eaf02b5107b8d062d9fb0b32dd3
MetehanBatal <metehanbatal@gmail.com>
2022-02-09 02:48:09 +0300

    Form handler

--- brief-stepped.js
+++ brief-stepped.js
@@ -177,7 +177,7 @@ let briefHench = {
 		inputs.forEach(function(input) {
 			let inputName = input.getAttribute('name');
 			self.formSchema.inputName = input.value;
-			console.log( self.formSchema.inputName, input.value );
+			console.log( inputName, input.value );
 			console.log( 'Schema: ', self.formSchema );
 		});
 	},
