f41570902018026f7358897d6f92ca9ae6201402
MetehanBatal <metehanbatal@gmail.com>
2022-02-08 08:45:05 +0300

    Form handler

--- brief-stepped.js
+++ brief-stepped.js
@@ -1,14 +1,66 @@
 let briefHench = {
+	stepCount: 1,
 	currentStep: 0,
 
-	setSteps: function() {
+
+	getSteps: function() {
 		const self = this;
 
-		let totalSteps = document.querySelectorAll('.step');
-		console.log( totalSteps, totalSteps.length );
+		let totalSteps = $('.step').length;
+
+		self.stepCount = totalSteps;
+	},
+
+	handleNext: function() {
+		const self = this;
+
+		// Reveal back button
+		// 
+		$('.to-previous-step').show();
+
+		self.currentStep += 1;
+
+		// Change URL to allow users to swipe back
+		// 
+		window.location.hash = `step=${self.stepCount}`;
+
+		$('.brief-stepped-form').addClass('hidden');
+		$('.brief-stepped-form')[self.currentStep].removeClass('hidden');
+	},
+
+	handleBack: function() {
+		const self = this;
+
+		if (self.currentStep === 0) {
+			$('.to-previous-step').hide();
+			return; }
+
+		self.currentStep -= 1;
+
+		// Change URL to allow users to swipe back
+		// 
+		window.location.hash = `step=${self.stepCount}`;
+
+		$('.brief-stepped-form').addClass('hidden');
+		$('.brief-stepped-form')[self.currentStep].removeClass('hidden');
+	}, 
+
+	listenStepChange: function() {
+		const self = this;
+
+		let nextButton = $('.to-next-step');
+		nextButton.click(function(e) {
+			self.handleNext();
+		});
+
+		let backButton = $('.to-previous-step');
+		backButton.click(function(e) {
+			self.handleBack();
+		});
 	}
 }
 
 $( document ).ready(function(e) {
 	briefHench.setSteps();
+	briefHench.listenStepChange();
 });
