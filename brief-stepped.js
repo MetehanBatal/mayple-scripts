252d7ed03cbe8834f6a677332754b64e8f620f19
MetehanBatal <metehanbatal@gmail.com>
2022-02-10 16:28:56 +0300

    Form handler

--- brief-stepped.js
+++ brief-stepped.js
@@ -59,6 +59,7 @@ let briefHench = {
 		const self = this;
 		$('.channel-selection .w--redirected-checked').each(function(index, el) {
 			let selectedSkill = $(this).parent().attr('skill-type');
+			console.log( 'Skill: ', selectedSkill );
 			self.formSchema['serviceTypes'].push(selectedSkill);
 		});
 	},
