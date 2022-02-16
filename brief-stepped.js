6ddedfde67b7da4b0a72f4343279c3527f6b8e66
MetehanBatal <metehanbatal@gmail.com>
2022-02-16 12:32:00 +0300

    Form handler

--- brief-stepped.js
+++ brief-stepped.js
@@ -85,7 +85,7 @@ let briefHench = {
 	insertSDK: function() {
 		const self = this;
 		const WebsiteSDK = window.WebsiteSDK.default;
-		self.websiteSDK = new WebsiteSDK({ environment: 'storky' });
+		self.websiteSDK = new WebsiteSDK();
 
 		console.log('SDK: ', self.websiteSDK);
 	},
