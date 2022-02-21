976285fcffa8495ec735e10b1636ffb2fa2a1271
MetehanBatal <metehanbatal@gmail.com>
2022-02-21 23:19:28 +0300

    Form handler

--- brief-stepped.js
+++ brief-stepped.js
@@ -601,6 +601,7 @@ window.addEventListener("message", function(e) {
 	if (!e.origin === 'https://meetings.hubspot.com') { return; }
 	if (e.data.meetingBookSucceeded) {
 		briefHench.reportWizardBriefStepDone('Wizard.Brief.Call Scheduled');
-		window.location.href = 'https://mayple.com/thank-you?name=' + briefHench.formSchema[firstName];
+		console.log( briefHench.formSchema );
+		window.location.href = 'https://mayple.com/thank-you?name=' + briefHench.formSchema['firstName'];
 	}
 });
