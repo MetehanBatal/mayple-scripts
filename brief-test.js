8c191f03fd5520eda94ea0ee52beaf63943140e3
MetehanBatal <metehanbatal@gmail.com>
2022-02-21 10:34:50 +0300

    Form handler

--- brief-test.js
+++ brief-test.js
@@ -525,5 +525,5 @@ window.addEventListener("message", function(e) {
 	}
 });
 
-document.getElementById('phone').addEventListener('change', briefHench.validatePhone());
-document.getElementById('phone').addEventListener('keyup', briefHench.validatePhone());
+// document.getElementById('phone').addEventListener('change', briefHench.validatePhone());
+// document.getElementById('phone').addEventListener('keyup', briefHench.validatePhone());
