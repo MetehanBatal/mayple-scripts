935d9f3adb1f9814c68de6a8bf07959122c45da8
MetehanBatal <metehanbatal@gmail.com>
2022-03-22 16:31:55 +0300

    Form handler

--- brief-stepped_lp.js
+++ brief-stepped_lp.js
@@ -766,6 +766,7 @@ $('.channel-selection input').bind('change', function(e) {
 		console.log( $('.channel-selection .w--redirected-checked') );
 		if ($('.channel-selection .w--redirected-checked').length > 0) {
 			$('.to-next-step').removeClass('disabled');
+			$('.to-next-step').show();
 			$('.to-next-step').click();
 		} else {
 			$('.to-next-step').addClass('disabled');
