bdb35579010c98a1df572f17dbf750fc16a05d4d
MetehanBatal <metehanbatal@gmail.com>
2022-05-22 13:53:03 +0300

    Form handler

--- brief-test.js
+++ brief-test.js
@@ -121,17 +121,17 @@ let briefHench = {
 
 		let iti = self.intlTel;
 		if (iti.isValidNumber()) {
-			return iti.getNumber(); }
+			return iti.getNumber(intlTelInputUtils.numberFormat.E164); }
 		else { return false; }
 	},
 
 	initIntlTel: function() {
 		const self = this;
 		let phoneInput = $("#phoneNumber");
+		console.log( 'Phone: ', phoneInput );
 		self.intlTel = window.intlTelInput(phoneInput, {
 			initialCountry: "auto",
 			utilsScript: "//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/utils.js",
-			hiddenInput: "full_phone",
 			geoIpLookup: function(success, failure) {
 				$.get("https://ipinfo.io?token=1fa95a0e3e5a98", function() {}, "jsonp").always(function(resp) { success((resp && resp.country) ? resp.country : "us"); });
 			},
