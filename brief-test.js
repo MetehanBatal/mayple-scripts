2b45d9779867a1687a1295fc1136b264302c266a
MetehanBatal <metehanbatal@gmail.com>
2022-05-24 11:17:14 +0300

    Form handler

--- brief-test.js
+++ brief-test.js
@@ -244,14 +244,22 @@ $(document).ready(function(e) {
 
 
 $('#marketingbudget').keyup(function(e) {
-	const isNumber = Number(e.originalEvent.key) ? true : false;
-	if (e.originalEvent.keyCode != 188 && e.originalEvent.keyCode != 8 && e.originalEvent.keyCode != 48 && e.originalEvent.keyCode != 13 && e.originalEvent.keyCode != 37 && e.originalEvent.keyCode != 39) {
-		if (!isNumber) {
-			let value = $('#marketingbudget').val().slice(0, -1);
-			$('#marketingbudget').val(value);
-			return;
-		}
+	let lastChar = $('#marketingbudget').val().slice(-1);
+	console.log( lastChar );
+	
+	if (isNaN(lastChar)) {
+		return;
 	}
+
+
+	// const isNumber = Number(e.originalEvent.key) ? true : false;
+	// if (e.originalEvent.keyCode != 188 && e.originalEvent.keyCode != 8 && e.originalEvent.keyCode != 48 && e.originalEvent.keyCode != 13 && e.originalEvent.keyCode != 37 && e.originalEvent.keyCode != 39) {
+	// 	if (!isNumber) {
+	// 		let value = $('#marketingbudget').val().slice(0, -1);
+	// 		$('#marketingbudget').val(value);
+	// 		return;
+	// 	}
+	// }
 
 	briefHench.restructureBudget();
 });
