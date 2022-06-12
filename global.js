45c9b1326457ea263acc0a6b21944f0cc39200a4
MetehanBatal <metehanbatal@gmail.com>
2022-06-13 02:07:52 +0300

    Form handler

--- global.js
+++ global.js
@@ -1,7 +1,8 @@
 let globalHench = {
 	loadWistia: function(array) {
 		array.forEach(function(video) {
-			const { url, container, lazyLoad } = object.url, object.container, object.lazyLoad
+			console.log( 'Video: ', video );
+			const { url, container, lazyLoad } = { video.url, video.container, video.lazyLoad };
 		});
 	}
 }
